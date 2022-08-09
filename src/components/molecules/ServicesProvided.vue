84d617e6949e862815e840df5622cd3df8f20a40
Sabrina <sabrinath1@hotmail.com>
2022-08-09 17:06:44 -0300

    services provided components

---
+++ src/components/molecules/ServicesProvided.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="services-provided">
+    <ServiceItem title="Front End" description="" />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor unde eum
+    impedit et harum modi amet, voluptatibus odit, molestias error tempora
+    dolore nemo quod vitae, at a ab mollitia ipsum.
+    <ServiceItem title="Back End" description="" />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor unde eum
+    impedit et harum modi amet, voluptatibus odit, molestias error tempora
+    dolore nemo quod vitae, at a ab mollitia ipsum.
+    <ServiceItem title="Manutenção" description="" />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor unde eum
+    impedit et harum modi amet, voluptatibus odit, molestias error tempora
+    dolore nemo quod vitae, at a ab mollitia ipsum.
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import ServiceItem from "@/components/atoms/ServiceItem.vue"
+
+export default defineComponent({ components: { ServiceItem } })
+</script>
+
+<style lang="scss" scoped></style>
